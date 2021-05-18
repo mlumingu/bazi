4409b203873aa03561ed7c8f1648400d453eb0f3
michael lumingu <michael.lumingu@ugent.be>
2021-05-18 21:40:53 +0200

    deploy

--- js/compiled/cljs-runtime/bazicljs.events.js
+++
@@ -1,6 +0,0 @@
-goog.provide('bazicljs.events');
-re_frame.core.reg_event_db.cljs$core$IFn$_invoke$arity$2(new cljs.core.Keyword("bazicljs.events","initialize-db","bazicljs.events/initialize-db",1348227589),(function (_,___$1){
-return bazicljs.db.default_db;
-}));
-
-//# sourceMappingURL=bazicljs.events.js.map
--- js/compiled/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('app');
-
-try { bazicljs.core.init(); } catch (e) { console.error("An error occurred when calling (bazicljs.core/init)"); throw(e); }
